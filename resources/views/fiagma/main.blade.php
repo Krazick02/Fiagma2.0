42790023a26e09b9e2f5b51217a3e906d80eb475
Angel Aviles <elangelcotaymas@gmail.com>
2023-05-29 20:24:09 -0700

    s

--- resources/views/fiagma/main.blade.php
+++ resources/views/fiagma/main.blade.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
-    <link rel="stylesheet" href="{{asset('asset/css/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg">
