9fe793c728e837ef33797f77b2f14b94bf9d975b
JArmandoTGProMX <armandopromx@gmail.com>
2023-06-06 19:51:21 -0700

    Cards_Styles

--- resources/views/fiagma/home.blade.php
+++ resources/views/fiagma/home.blade.php
@@ -6,6 +6,7 @@
     <title>Home</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
     <link rel="stylesheet" href="{{asset('assets/css/styles.css')}}">
+    <link rel="stylesheet" href="{{asset('assets/css/cards_styles.css')}}">
 </head>
 <body>
   <nav class="navbar navbar-expand-lg">
@@ -14,7 +15,7 @@
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
-        <div class="collapse navbar-collapse" id="navbarSupportedContent">  
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0 navbar-nav-right">
             <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
@@ -58,30 +59,40 @@
         <hr class="border border-danger border-1 opacity-50" >
       </div>
 
-      <div style="width: 100%;">
-        <div class="container text-center">
-          <div class="row row-cols-2 row-cols-lg-3 g-2 g-lg-3">
-            <div class="col">
-              <div class="p-3 bg-secondary opacity-50  " style="width: 100%; height: 100px ">proyecto 1</div>
+      <div class="card-group">
+        <div class="card">
+            <img src="./src/images/gray.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Fecha Creacion: 11/Mayo/2023</p>
+              <a href="#" class="btn btn-primary">Iniciar Proyecto</a>
             </div>
-            <div class="col">
-              <div class="p-3 bg-secondary opacity-50 " style="width: 100%; height: 100px ">proyecto 1</div>
-            </div>
-            <div class="col">
-              <div class="p-3 bg-secondary opacity-50 " style="width: 100%; height: 100px ">proyecto 1</div>
-            </div>
-            <div class="col">
-              <div class="p-3 bg-secondary opacity-50 " style="width: 100%; height: 100px ">proyecto 1</div>
-            </div>
-            <div class="col">
-              <div class="p-3 bg-secondary opacity-50 " style="width: 100%; height: 100px ">proyecto 1</div>
-            </div>          
-            <div class="col">
-              <div class="p-3 bg-secondary opacity-50 " style="width: 100%; height: 100px ">proyecto 1</div>
-            </div>          
-          </div>
         </div>
-      </div>
+        <div class="card">
+            <img src="./src/images/gray.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Fecha Creacion: 11/Mayo/2023</p>
+              <a href="#" class="btn btn-primary">Iniciar Proyecto</a>
+            </div>
+        </div>
+        <div class="card" >
+            <img src="./src/images/gray.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Fecha Creacion: 11/Mayo/2023</p>
+              <a href="#" class="btn btn-primary">Iniciar Proyecto</a>
+            </div>
+        </div>
+        <div class="card">
+            <img src="./src/images/gray.jpg" class="card-img-top" alt="...">
+            <div class="card-body">
+              <h5 class="card-title">Card title</h5>
+              <p class="card-text">Fecha Creacion: 11/Mayo/2023</p>
+              <a href="#" class="btn btn-primary">Iniciar Proyecto</a>
+            </div>
+        </div>
+    </div>
 
     </div>
 
